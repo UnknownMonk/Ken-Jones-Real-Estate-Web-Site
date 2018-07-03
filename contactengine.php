13eeb0dee414fed2b4bff0b92c2635ab73bb8299
UnknownMonk <smodcartoon@gmail.com>
2018-07-03 09:25:44 -0400

    compressed scss

--- contactengine.php
+++ contactengine.php
@@ -35,7 +35,7 @@ $success = mail($EmailTo, $Subject, $Body, "From: <$EmailFrom>");
 
 // redirect to success page 
 if ($success){
-  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=payPal.html\">";
 }
 else{
   print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm\">";
